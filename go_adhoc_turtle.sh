a8ba1e5c1a3bd717053ccd854a967fd6ec7373bb
Daniel D'Souza <dnldsz9@gmail.com>
2022-02-10 11:42:03 -0800

    modified shell script

--- go_adhoc_turtle.sh
+++ go_adhoc_turtle.sh
@@ -1,3 +1,7 @@
+#Change this to turtlebot number
+IPADDR="192.168.7.2/24"
+
+
 sudo service network-manager stop
 sleep 1
 sudo ip link set wlan0 down
@@ -6,4 +10,4 @@ sudo iwconfig wlan0 mode ad-hoc
 sudo iwconfig wlan0 essid 'TurtleNet'
 sudo ip link set wlan0 up
 sleep 1
-sudo ip addr add 192.168.7.1/24 dev wlan0
+sudo ip addr add $IPADDR dev wlan0
