7a99d84e4ff1b353edd17e378a96be182a5b699c
lilliamc <lilliamc@usc.edu>
2022-02-24 15:50:29 -0800

    updated config files

--- pc_tools/pc_adhoc.sh
+++ pc_tools/pc_adhoc.sh
@@ -1,5 +1,5 @@
 #change this correct wireless interface(type iwconfig in your terminal)
-WIRELESS_INTERFACE="wlx1cbfce96f922"
+WIRELESS_INTERFACE="wlx1cbfce95c8ee"
 
 sudo ip link set $WIRELESS_INTERFACE down 
 
@@ -7,6 +7,6 @@ sudo iwconfig $WIRELESS_INTERFACE mode ad-hoc
 
 sudo ip link set $WIRELESS_INTERFACE up
 
-sudo ip addr add 192.168.7.10/24 dev $WIRELESS_INTERFACE
+sudo ip addr add 192.168.7.11/24 dev $WIRELESS_INTERFACE
 
 sudo iwconfig $WIRELESS_INTERFACE essid "TurtleNet"
