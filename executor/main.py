d98b964346f46a9996e58e7416e39ff4fb98bb16
Jared Ray Coleman <jaredraycoleman@gmail.com>
2022-03-28 15:09:05 +0000

    Adds network

--- executor/main.py
+++ executor/main.py
@@ -1,5 +1,8 @@
-from typing import Dict, List, Set, Any
-from task_graph import TaskGraph, graph, deserialize, serialize
+from typing import Dict, Iterable, List, Set, Any, Generator, Tuple
+from uuid import uuid4
+
+import numpy as np
+from task_graph import TaskGraph, deserialize, serialize
 import json
 
 class Node:
@@ -9,7 +12,7 @@ class Node:
         self.data: Dict[str, str] = {}
         self.execution_history: Dict[str, Set] = {}
 
-    def process_message(self, msg_str: str):
+    def process_message(self, msg_str: str) -> Generator[Tuple[str, str], None, None]:
         msg: Dict[str, Any] = json.loads(msg_str)
         execution_id: str = msg["execution_id"]
 
@@ -42,6 +45,8 @@ class Node:
                 schedule[other_task] for other_task, deps in task_graph.items()
                 if task in deps
             }
+            if not next_nodes:
+                print(f"OUTPUT: {task}: {deserialize(task_output)}")
             for next_node in next_nodes:
                 msg = json.dumps(
                     {
@@ -53,26 +58,77 @@ class Node:
                     indent=2
                 )
                 if self.name == next_node:
-                    self.process_message(msg) 
+                    yield from self.process_message(msg) 
                 else:
-                    print(f"SEND {self.name} -> {next_node}:\n", msg)
+                    yield next_node, msg
+
+class Network:
+    def __init__(self, nodes: Iterable[Node]) -> None:
+        self.nodes = {node.name: node for node in nodes}
+
+    def send(self, src_node_name: str, dst_node_name: str, message: str) -> None:
+        print(f"SENDING DATA FROM {src_node_name} -> {dst_node_name}")
+        for next_node, output in self.nodes[dst_node_name].process_message(message):
+            self.send(dst_node_name, next_node, output)
+
+    def execute(self, graph: TaskGraph, schedule: Dict[str, str]) -> Any:
+        message = json.dumps({
+            "execution_id": uuid4().hex,
+            "data": {},
+            "task_graph": graph.summary(),
+            "schedule": schedule
+        })
+
+        for task in graph.start_tasks():
+            node = schedule[task]
+            for next_node, output in self.nodes[node].process_message(message):
+                self.send(node, next_node, output)
+
+def get_graph():
+    graph = TaskGraph()
+    @graph.task()
+    def generate_data() -> np.array:
+        return np.zeros(100)
+
+    @graph.task(generate_data)
+    def add_noise(arr: np.ndarray) -> np.array:
+        return arr + np.random.random(arr.shape)
+
+    @graph.task(add_noise)
+    def mean(arr: np.ndarray) -> float:
+        return np.mean(arr)
+        
+    @graph.task(add_noise)
+    def min(arr: np.ndarray) -> float:
+        return np.min(arr)
+
+    @graph.task(add_noise)
+    def max(arr: np.ndarray) -> float:
+        return np.max(arr)
+
+    @graph.task(min, max)
+    def midpoint(arr_min: float, arr_max: float) -> float:
+        return (arr_min + arr_max) / 2
+
+    return graph
 
 def main():
-    node = Node("node_1", graph)
-    message = {
-        "execution_id": "asdf",
-        "data": {},
-        "task_graph": graph.summary(),
-        "schedule": {
-            "generate_data": "node_1",
-            "mean": "node_1",
-            "min": "node_2",
-            "max": "node_3",
-            "midpoint": "node_3",
-            "add_noise": "node_1"
-        }
-    }
-    node.process_message(json.dumps(message))
+    graph = get_graph()
+    network = Network(
+        nodes=[
+            Node(f"node_{i}", graph=graph) 
+            for i in range(1, 5)
+        ]
+    )
+
+    network.execute(graph, schedule={
+        "generate_data": "node_1",
+        "add_noise": "node_1",
+        "mean": "node_2",
+        "min": "node_1",
+        "max": "node_2",
+        "midpoint": "node_3",
+    })
 
 if __name__ == "__main__":
     main()
--- executor/task_graph.py
+++ executor/task_graph.py
@@ -20,7 +20,7 @@ class Task:
 
 class TaskGraph:
     def __init__(self) -> None:
-        self.tasks: Dict[Task, List[Task]] = {}
+        self.task_deps: Dict[Task, List[Task]] = {}
         self.task_names: Dict[str, Task] = {}
 
     def execute(self, name: str, *args, **kwargs) -> Any:
@@ -29,56 +29,49 @@ class TaskGraph:
     def dependencies(self, name: str) -> List[str]:
         return [
             task.name for task in
-            self.tasks[self.task_names[name]]
+            self.task_deps[self.task_names[name]]
         ]
-    
+
     def task(self, *deps: Task) -> Callable:
-        def _task(fun: Callable) -> Task:
-            def _fun(*args, **kwargs) -> Any:
-                args = [deserialize(arg) for arg in args]
-                kwargs = {key: deserialize(value) for key, value in kwargs.items()}
-                return serialize(fun(*args, **kwargs))
-            task = Task(fun.__name__, _fun)
-            self.tasks[task] = deps
-            self.task_names[task.name] = task
-            return task
+        def _task(fun: Callable) -> Any:
+            return self.add_task(fun, *deps)
         return _task
+        
+    def add_task(self, fun: Callable, *deps: Task) -> Task:
+        def _fun(*args, **kwargs) -> Any:
+            args = [deserialize(arg) for arg in args]
+            kwargs = {
+                key: deserialize(value) 
+                for key, value in kwargs.items()
+            }
+            return serialize(fun(*args, **kwargs))
+        task = Task(fun.__name__, _fun)
+        self.task_deps[task] = deps
+        self.task_names[task.name] = task
+        return task
 
     def __str__(self) -> str:
         return "\n".join([
             f"{task.name} <- [{', '.join([dep.name for dep in deps])}]"
-            for task, deps in self.tasks.items()
+            for task, deps in self.task_deps.items()
         ])
+
+    def start_tasks(self) -> List[str]:
+        return [
+            task_name for task_name, task in self.task_names.items()
+            if not self.task_deps[task]
+        ]
+
+    def end_tasks(self) -> List[str]:
+        source_tasks = {
+            dep.name for _, task in self.task_names.items() 
+            for dep in self.task_deps[task]
+        }
+        return list(set(self.task_names.keys()) - source_tasks)
 
     def summary(self) -> Dict[str, List[str]]:
         return {
             task.name: [dep.name for dep in deps]
-            for task, deps in self.tasks.items()
+            for task, deps in self.task_deps.items()
         }
 
-graph = TaskGraph()
-
-
-@graph.task()
-def generate_data() -> np.array:
-    return np.zeros(100)
-
-@graph.task(generate_data)
-def add_noise(arr: np.ndarray) -> np.array:
-    return arr + np.random.random(arr.shape)
-
-@graph.task(add_noise)
-def mean(arr: np.ndarray) -> float:
-    return np.mean(arr)
-    
-@graph.task(add_noise)
-def min(arr: np.ndarray) -> float:
-    return np.min(arr)
-
-@graph.task(add_noise)
-def max(arr: np.ndarray) -> float:
-    return np.max(arr)
-
-@graph.task(min, max)
-def midpoint(arr_min: float, arr_max: float) -> float:
-    return (arr_min + arr_max) / 2
