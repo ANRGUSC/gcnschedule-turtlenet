40bda23550191218e46981a2eab8ba499de82548
lilliamc <lilliamc@usc.edu>
2022-04-20 14:30:44 -0700

    pings are on pub/sub too

--- src/gcnsched_demo/gcnsched_demo/bandwidth_node.py
+++ src/gcnsched_demo/gcnsched_demo/bandwidth_node.py
@@ -1,78 +1,74 @@
 import asyncio
-from interfaces.srv import Bandwidth
-from std_msgs.msg import Float64
+# from interfaces.srv import Bandwidth
+from interfaces.msg import StringStamped
+from std_msgs.msg import Float64, Int32
 import rclpy
 from rclpy.node import Node, Client, Publisher
-from rclpy.callback_groups import ReentrantCallbackGroup
-from rclpy.executors import MultiThreadedExecutor
+# from rclpy.callback_groups import ReentrantCallbackGroup
+# from rclpy.executors import MultiThreadedExecutor
 import time
 from functools import partial
 from typing import List
 import os
 from rclpy.exceptions import ParameterNotDeclaredException
 from rcl_interfaces.msg import ParameterType
-
+from rclpy.time import Time
 
 class BandwidthNode(Node):
     def __init__(self, name: str, other_nodes: List[str], interval: float) -> None:
         super().__init__("bandwidth") #f"{name}_bandwidth")
+        self.PING_MSG = "hello"
+
         self.declare_parameter('name', 'default_node')
         self.declare_parameter('other_nodes',[])
         self.interval = interval
         print("INTERVAL", interval)
         name = self.get_parameter('name').get_parameter_value().string_value
-        other_nodes = self.get_parameter('other_nodes').get_parameter_value().string_array_value
+        self.other_nodes = self.get_parameter('other_nodes').get_parameter_value().string_array_value
         self.get_logger().info(f"INIT {name}")
-        if not other_nodes:
+        if not self.other_nodes:
             self.get_logger.info("Error: could not find other nodes")
 
-        cb_group = ReentrantCallbackGroup()
-        self.ping_service = self.create_service(
-            Bandwidth, 'ping',
-            self.ping_callback,
-            callback_group=cb_group
-        )
+        my_ns = self.get_namespace().split("/")[-1]
+        self.request_topics = {}
+        self.response_topics = {}
+        self.publish_topics = {}
+        for other_node in self.other_nodes:
+            self.create_subscription(StringStamped, f"/{other_node}/{my_ns}/ping",partial(self.send_reply, other_node, my_ns))
+            self.create_subscription(StringStamped, f"/{my_ns}/{other_node}/reply",partial(self.publish_metric, other_node, my_ns))
+            self.request_topics[other_node] = self.create_publisher(StringStamped, f"/{my_ns}/{other_node}/ping")
+            self.response_topics[other_node] = self.create_publisher(StringStamped, f"/{other_node}/{my_ns}/reply")
+            self.publish_topics[other_node] = self.create_publisher(Float64, f"/{my_ns}/{other_node}/bandwidth")
 
-        self.name = name
-        for other_node in other_nodes:
-            cli = self.create_client(Bandwidth, f"/{other_node}/ping", callback_group=cb_group)
-            pub = self.create_publisher(Float64, f"{other_node}/bandwidth", 10, callback_group=cb_group)
-            while not cli.wait_for_service(timeout_sec=1.0):
-                self.get_logger().warning(f'service {other_node}/ping not available, waiting again...')
-            self.create_timer(interval, partial(self.ping_node, cli, pub), callback_group=cb_group)
+        self.create_timer(interval, self.ping_node)
 
-    def publish_ping(self, start: float, pub: Publisher, req_length: int, *args, **kwargs) -> None:
-        self.get_logger().info("UNSTUCK")
-        dt = time.time() - start
-        self.get_logger().info(f'TIME For Ping service:{dt}')
-        msg = Float64()
-        msg.data = (req_length / dt)/125 # take message size into account
-        self.get_logger().info("publishing")
-        pub.publish(msg)
+    def ping_node(self):
+        # ping everyone (requests)
+        for other_node in self.other_nodes:
+            s = StringStamped()
+            s.header.stamp = self.get_clock().now().to_msg()
+            s.string = self.PING_MSG
+            self.request_topics[other_node].publish(s)
 
-    def ping_node(self, cli: Client, pub: Publisher) -> None:
-        self.get_logger().info("Inside PING NODE")
-        MSG = "hello"*1000
-        req_length = len(MSG.encode("utf-8"))
-        req = Bandwidth.Request()
-        req.a = MSG
-        self.get_logger().info("STUCK")
-        #TODO: fix the wait for service
-        temp_start = time.time()
-        if cli.wait_for_service(timeout_sec=self.interval/2):
-            self.get_logger().info(f'Time until service is ready: {(time.time() - temp_start)}')
-            start = time.time()
-            fut = cli.call_async(req)
-            fut.add_done_callback(partial(self.publish_ping, start, pub, req_length))
-        else: #if wait for service failed
-            self.get_logger().info(f'Time it took to FAIL: {(time.time() - temp_start)}')
-            self.get_logger().info("****FAILED*****")
+    def send_reply(self, other_node, my_ns, msg):
+        # reply to a ping request
+        s = StringStamped()
+        s.header = msg.header # repeat header, so reply has time of initial sent
+        s.string = msg.string
+        self.response_topics[other_node].publish(s)
 
-    def ping_callback(self,
-                      request: Bandwidth.Request,
-                      response: Bandwidth.Response) -> Bandwidth.Response:
-        response.b = request.a
-        return response
+    def publish_metric(self, other_node, my_ns, msg):
+        # process a ping reply
+        self.get_logger().info(str(self.get_clock().now()))
+        self.get_logger().info(str(msg.header.stamp))
+        dt =  self.get_clock().now() - Time.from_msg(msg.header.stamp)
+        dt_secs = dt.nanoseconds/1e9
+        self.get_logger().info(f'TIME For Ping roundtrip:{dt_secs}')
+        f = Float64()
+        req_length = len(self.PING_MSG.encode("utf-8"))
+        f.data = (req_length / dt_secs)/125
+        self.publish_topics[other_node].publish(f)
+
 
 def main(args=None):
     rclpy.init(args=args)
@@ -86,10 +82,6 @@ def main(args=None):
         interval=5
     )
 
-    # executor = MultiThreadedExecutor(num_threads=1000)
-    # executor.add_node(bandwidth_client_node)
-
-    # executor.spin()
     rclpy.spin(bandwidth_client_node)
     bandwidth_client_node.destroy_node()
     rclpy.shutdown()
